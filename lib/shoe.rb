c52091d261774f48b06d0cc564458868ce501fc6
Justin Winger <justinwinger4@gmail.com>
2020-03-30 16:39:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+def Shoe
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand=(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  
+
+  
+end 
